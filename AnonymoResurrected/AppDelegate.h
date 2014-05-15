035477fb3e2e52fee33a467a53ca749c99a85f0c
ionsilviumihai <silviu.ion@sparktech.ro>
2014-05-15 11:13:32 +0300

    Initial Commit

---
+++ AnonymoResurrected/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnonymoResurrected
+//
+//  Created by Ion Silviu-Mihai on 15/05/14.
+//  Copyright (c) 2014 sparktech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AnonymoResurrected/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AnonymoResurrected
+//
+//  Created by Ion Silviu-Mihai on 15/05/14.
+//  Copyright (c) 2014 sparktech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
